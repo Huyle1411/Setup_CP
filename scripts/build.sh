1d776e836812b7d9605d80461c77041e0dafd91a
Huyle1411 <44628783+Huyle1411@users.noreply.github.com>
2022-02-15 10:48:29 +0700

    Create build.sh

---
+++ scripts/build.sh
@@ -0,0 +1,1 @@
+g++ -g -std=gnu++2a -D_DEBUG -O2 -Wall -Wextra -pedantic -Wshadow -Wformat=2 -Wfloat-equal -Wconversion -Wlogical-op -Wshift-overflow=2 -Wduplicated-cond -Wcast-qual -Wcast-align -Wno-unused-result -Wno-sign-conversion -o $1 $1.cpp
