3d2d1f176e766f68baf1a4f2ad07b96c560ef3e4
huyle1411 <44628783+Huyle1411@users.noreply.github.com>
2022-03-17 17:47:48 +0700

    Update build.sh

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,1 +1,1 @@
-g++ -g -std=gnu++2a -D_DEBUG -O2 -Wall -Wextra -pedantic -Wshadow -Wformat=2 -Wfloat-equal -Wconversion -Wlogical-op -Wshift-overflow=2 -Wduplicated-cond -Wcast-qual -Wcast-align -Wno-unused-result -Wno-sign-conversion -o $1 $1.cpp
+g++ -g -std=gnu++2a -DDEBUG -O2 -Wall -Wextra -pedantic -Wshadow -Wformat=2 -Wfloat-equal -Wconversion -Wlogical-op -Wshift-overflow=2 -Wduplicated-cond -Wcast-qual -Wcast-align -Wno-unused-result -Wno-sign-conversion -o $1 $1.cpp
