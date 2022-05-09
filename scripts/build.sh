80c18c90e7111b1527010a1ea1e3046a1d58ff04
huyle1411 <44628783+Huyle1411@users.noreply.github.com>
2022-05-09 17:25:35 +0700

    update

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,24 +1,67 @@
 #!/bin/bash
+usage()
+{
+	echo "Usage: $(basename $0) <filename> [-d] [-c] [-r <number>]" 1>&2 
+	exit 1
+}
+
+RED='\033[0;31m'
+PURPLE='\033[0;35m'
+GREEN='\033[0;32m'
+NC='\033[0m' # No Color
+
+if [[ "$1" == "-h" ]]; then
+	usage
+fi
+
 filename=$(basename -- "$1")
 extension="${filename##*.}"
 filename="${filename%.*}"
+shift
 
 CXXFLAGS="-g -std=c++2a -O2 -Wall -Wextra -pedantic -Wshadow -Wformat=2 -Wfloat-equal -Wconversion -Wlogical-op -Wshift-overflow=2 -Wduplicated-cond -Wcast-qual -Wcast-align -Wno-unused-result -Wno-sign-conversion"
 DEBUG_CXXFLAGS="-fsanitize=address -fsanitize=undefined -fsanitize=float-divide-by-zero -fsanitize=float-cast-overflow -fno-sanitize-recover=all -fstack-protector-all -D_FORTIFY_SOURCE=2"
 #DEBUG_LOCAL_CXXFLAGS="-D_GLIBCXX_DEBUG -D_GLIBCXX_DEBUG_PEDANTIC -DDEBUG -I/home/huyle/library/debug"
 DEBUG_LOCAL_CXXFLAGS="-DDEBUG -I/home/huyle/library/debug"
 
-DEBUG=1
+DEBUG=0
+COMPILE_ONLY=0
+NUMBER_LOOP=1
 
-if [[ "$2" == "--nodebug" || "$2" == "-nd" ]]; then
-	DEBUG=0
-fi
+while getopts ":hdcr:" option; do
+	case "$option" in
+		h)
+			usage
+			;;
+		d)
+			DEBUG=1
+			;;
+		c)
+			COMPILE_ONLY=1
+			;;
+		r)
+			NUMBER_LOOP="$OPTARG"
+			;;
+	esac
+done
 
 if [[ $DEBUG -eq 1 ]]; then
-	echo "the DEBUG flags is on"
+	echo -e "${PURPLE}the DEBUG flags is on${NC}"
 	CXXFLAGS+=" ${DEBUG_LOCAL_CXXFLAGS}"
 fi
 
-g++ $CXXFLAGS -o $filename.out $filename.cpp
+echo -e "${GREEN}Compiling...${NC}"
+output=$(g++ $CXXFLAGS -o $filename.out $filename.cpp)
+if [[ $? != 0 ]]; then
+	echo -e "${RED}***ERROR***${NC}"
+elif [[ $COMPILE_ONLY -eq 1 ]]; then
+	echo -e "${GREEN}Done${NC}"
+else
+	for i in $( eval echo {1..$NUMBER_LOOP} ); do
+		echo -e "${GREEN}Running...${NC}"
+		./$filename.out ;
+	done
+fi
 
 #g++ -g -std=c++2a -I/home/huyle/library/debug -DDEBUG -O2 -Wall -Wextra -pedantic -Wshadow -Wformat=2 -Wfloat-equal -Wconversion -Wlogical-op -Wshift-overflow=2 -Wduplicated-cond -Wcast-qual -Wcast-align -Wno-unused-result -Wno-sign-conversion -o $1 $1.cpp
+
