73508d249c1a6c3a5b66784a1a7e52e924b22e51
huyle <lqh1411@gmail.com>
2022-08-17 21:14:20 +0700

    update

---
+++ .template/template.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using i64 = long long;
+#ifdef DEBUG
+#include "debug.h"
+#else
+#define debug(...) 69
+#endif
+
+int32_t main() {
+  int tt = 1;
+  scanf("%d", &tt);
+  while (tt--) {
+  }
+}
