395011c87705312f0eff4ebc08daab039453139f
huyle <lqh1411@gmail.com>
2022-08-17 21:14:01 +0700

    update

---
+++ scripts/make_problem_cmake.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+TEMPLATE_DIR='/home/huyle/.template/'
+
+for filepath in "$@"; do
+    PROBLEM_NAME=$(basename "$filepath")
+
+    if [[ -e "$filepath" ]]; then
+        echo "$filepath already exists. Remove it and retry."
+        continue
+    fi
+
+    # Copy files in
+    mkdir -p "$filepath"
+		cp -r "$TEMPLATE_DIR/"* "$filepath/"
+		mv "$filepath/"template.cpp "$filepath/$PROBLEM_NAME".cpp
+		echo "created $PROBLEM_NAME.cpp file"
+		
+		# rename PROBLEM_NAME in file CMakeLists.txt
+		sed -i "s/PROBLEM_NAME/$PROBLEM_NAME/g" "$filepath"/CMakeLists.txt
+		
+		# Run cmake
+		cd "$filepath/"
+		cmake .
+		cd ..
+done
+
