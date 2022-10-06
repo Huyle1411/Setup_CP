1c2dd362a52f4c2ca0a370685eb4d426ed33562c
huyle1411 <44628783+Huyle1411@users.noreply.github.com>
2022-10-06 16:21:57 +0700

    Update run_problem.sh

--- scripts/run_problem.sh
+++ scripts/run_problem.sh
@@ -20,32 +20,37 @@ right_answer=0
 test_case=1
 for input_file in *.in
 do
-	filename=$(basename -- "$input_file")
-	filename="${filename%.*}"
-	output_file="${filename}.res"
-	expected_file="${filename}.out"
-	./$execute_file < $input_file > $output_file
-	# if diff -w -B -F --label --side-by-side $expected_file $output_file > dont_show_on_terminal.txt; then
-  if diff -Z -B <(grep -vE '^\s*$' $expected_file) <(grep -vE '^\s*$' $output_file) > dont_show_on_terminal.txt; then
-		echo "Test case $test_case: ${bold}${green}Accepted${reset}"
-		right_answer=$((right_answer+1))
-		rm $output_file
-	else
-		echo "Test case $test_case: ${bold}${red}Wrong Answer${reset}"
-    echo "${blue}Input: ${reset}"
-		cat $input_file
-		echo ""
+  if [ -s input_file ]
+  then
+    filename=$(basename -- "$input_file")
+    filename="${filename%.*}"
+    output_file="${filename}.res"
+    expected_file="${filename}.out"
+    ./$execute_file < $input_file > $output_file
+    # if diff -w -B -F --label --side-by-side $expected_file $output_file > dont_show_on_terminal.txt; then
+    if diff -Z -B <(grep -vE '^\s*$' $expected_file) <(grep -vE '^\s*$' $output_file) > dont_show_on_terminal.txt; then
+      echo "Test case $test_case: ${bold}${green}Accepted${reset}"
+      right_answer=$((right_answer+1))
+      rm $output_file
+    else
+      echo "Test case $test_case: ${bold}${red}Wrong Answer${reset}"
+      echo "${blue}Input: ${reset}"
+      cat $input_file
+      echo ""
 
-    echo "${blue}Output: ${reset}${CUSTOMTAB}${blue}Expected: ${reset}"
-    colordiff -y <(grep -vE '^\s*$' $output_file) <(grep -vE '^\s*$' $expected_file)
-
+      echo "${blue}Output: ${reset}${CUSTOMTAB}${blue}Expected: ${reset}"
+      colordiff -y <(grep -vE '^\s*$' $output_file) <(grep -vE '^\s*$' $expected_file)
+    fi
+  else
+    echo "${orange}No test cases found!!!${reset}"
+    exit 0
+  fi
 		# cat $output_file
 		# echo ""
 		#
   #   echo "${blue}Expected: ${reset}"
 		# cat $expected_file
 		# echo ""
-	fi
 	test_case=$((test_case+1))
 done
 
