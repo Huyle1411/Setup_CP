ab6934cd9161247d5daa636833709fc0fda94ea8
huyle <huyle@KimAnh>
2023-02-05 13:15:53 +0700

    change format

--- .template/template.cpp
+++ .template/template.cpp
@@ -7,10 +7,10 @@ using i64 = long long;
 #endif
 
 int32_t main() {
-  std::cin.tie(0)->sync_with_stdio(0);
-  std::cin.exceptions(std::cin.failbit);
-  int tt = 1;
-  std::cin >> tt;
-  while (tt--) {
-  }
+    std::cin.tie(0)->sync_with_stdio(0);
+    std::cin.exceptions(std::cin.failbit);
+    int tt = 1;
+    std::cin >> tt;
+    while (tt--) {
+    }
 }
