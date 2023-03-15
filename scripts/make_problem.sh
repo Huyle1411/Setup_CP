51ff52b6867b3de65c03b4edfd68ccec6e79e0d7
huyle1411 <44628783+Huyle1411@users.noreply.github.com>
2023-03-15 16:18:22 +0700

    Update make_problem.sh

--- scripts/make_problem.sh
+++ scripts/make_problem.sh
@@ -5,26 +5,24 @@ TEMPLATE_DIR='/home/huyle/Setup_CP/.template'
 filepath=$1
 PROBLEM_NAME=$(basename "$filepath")
 ROOT_NAME=$2
-LANG="cpp"
+LANG=$3
 
-if [[ -e "$filepath" ]]; then
-    echo "$filepath already exists. Remove it and retry."
-    continue
-fi
+# if [[ -e "$filepath" ]]; then
+#     echo "$filepath already exists. Remove it and retry."
+#     continue
+# fi
 
-if [ $# -eq 2 ]
+if [ -z "$LANG" ]
   then
     LANG="cpp" #default
-  else
-    LANG=$3
 fi
 
-mkdir -p "$filepath"
+# mkdir -p "$filepath"
 
 if [ "$LANG" == "cpp" ]; then
   # Copy files
-  cp -r "$TEMPLATE_DIR/template.cpp" "$filepath/"
-  mv "$filepath/"template.cpp "$filepath/$PROBLEM_NAME".cpp
+  cp -r "$TEMPLATE_DIR/template.cpp" "./"
+  mv template.cpp "$PROBLEM_NAME".cpp
   echo "created $PROBLEM_NAME.cpp file"
 
   if [ "$PROBLEM_NAME" == "$ROOT_NAME" ]
@@ -35,7 +33,7 @@ if [ "$LANG" == "cpp" ]; then
 
   CMAKE_FILE="CMakeLists".txt
   echo "" >> $CMAKE_FILE
-  echo "add_executable($PROBLEM_NAME $PROBLEM_NAME/$PROBLEM_NAME.cpp)
+  echo "add_executable($PROBLEM_NAME $PROBLEM_NAME.cpp)
   target_include_directories($PROBLEM_NAME PRIVATE ./)" >> $CMAKE_FILE
 
   if [ "$PROBLEM_NAME" == "$ROOT_NAME" ]
@@ -54,6 +52,6 @@ elif [ "$LANG" == "java" ]; then
   echo "created $PROBLEM_NAME.java file"
 elif [ "$LANG" == "python" ]; then
   cp -r "$TEMPLATE_DIR/template.py" "$filepath/"
-  mv "$filepath/"template.py "$filepath/$PROBLEM_NAME".py
+  mv template.py "$PROBLEM_NAME".py
   echo "created $PROBLEM_NAME.py file"
 fi
